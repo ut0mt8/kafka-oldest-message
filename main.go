f23b601ae24cbd9ea4d5a97211a05303e977d78e
Raphael Mazelier <raph@futomaki.net>
2019-11-23 20:15:55 +0100

    A bit of factorisation

--- main.go
+++ main.go
@@ -4,45 +4,56 @@ import (
 	"fmt"
 	"github.com/Shopify/sarama"
 	"strings"
-        "time"
+	"time"
 )
+
+func getOffsetTimestamp(consumer sarama.Consumer, topic string, partition int32, offset int64) (time.Time, error) {
+	cp, err := consumer.ConsumePartition(topic, partition, offset)
+	if nil != err {
+		return time.Time{}, fmt.Errorf("cannot consumme partition: ", err)
+	}
+
+	select {
+	case <-time.After(500 * time.Millisecond):
+		return time.Time{}, fmt.Errorf("no message in topic")
+	case consumerError := <-cp.Errors():
+		return time.Time{}, fmt.Errorf("consumer error: ", consumerError.Err)
+	case msg := <-cp.Messages():
+		return msg.Timestamp, nil
+	}
+
+	return time.Time{}, fmt.Errorf("unknow error")
+}
 
 func main() {
 
 	config := sarama.NewConfig()
-        config.Version = sarama.V2_1_0_0
-	config.Consumer.Offsets.CommitInterval = 60
+	config.Version = sarama.V2_1_0_0
+	config.Consumer.Offsets.AutoCommit.Enable = false
 	config.Consumer.Return.Errors = true
 
 	brokers := []string{"localhost:9092"}
 
-	client, err := sarama.NewConsumer(brokers, config)
+	consumer, err := sarama.NewConsumer(brokers, config)
 	if err != nil {
 		panic(err)
 	}
 
-	topics, _ := client.Topics()
+	topics, _ := consumer.Topics()
 
 	for _, topic := range topics {
 		if strings.Contains(topic, "__consumer_offsets") {
 			continue
 		}
 
-		partitions, _ := client.Partitions(topic)
-		consumer, err := client.ConsumePartition(topic, partitions[0], sarama.OffsetOldest)
-		if nil != err {
-			panic(err)
-		}
-
-		select {
-		case consumerError := <-consumer.Errors():
-			fmt.Println("consumerError: ", consumerError.Err)
-		case msg := <-consumer.Messages():
-			fmt.Printf("oldest message in topic %s at %v\n", topic, msg.Timestamp)
-                case <-time.After(500 * time.Millisecond):
-			fmt.Printf("no message in topic %s\n", topic)
+		partitions, _ := consumer.Partitions(topic)
+		ts, err := getOffsetTimestamp(consumer, topic, partitions[0], sarama.OffsetOldest)
+		if err != nil {
+			fmt.Printf("cannot get oldest message in topic %s : %v\n", topic, err)
+		} else {
+			fmt.Printf("oldest message in topic %s at %v\n", topic, ts)
 		}
 	}
 
-	client.Close()
+	consumer.Close()
 }
